1cd12f28b5a03c90b5ab76494e0b3c3f43a71c7a
Thong Do <thongdo@WBs-MacBook-Pro.local>
2017-03-29 17:55:34 -0700

    Sendgrid php script added

--- test/mailtest.php
+++ test/mailtest.php
@@ -3,25 +3,20 @@
 // using SendGrid's PHP Library
 // https://github.com/sendgrid/sendgrid-php
 // If you are using Composer (recommended)
-require '/home/ec2-user/vendor/autoload.php';
+require 'vendor/autoload.php';
 // If you are not using Composer
 // require("path/to/sendgrid-php/sendgrid-php.php");
 $from = new SendGrid\Email("Thong Do", "thong@citybeauty.com");
-$subject = "test-mail from thong's Sendgrid";
+$subject = "New Form Submission";
+$email_body ="test body email";
 $to = new SendGrid\Email("Thong Do", "thong@citybeauty.com");
-$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
+$content = new SendGrid\Content("text/plain", $email_body);
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
-$apiKey = getenv('SENDGRID_API_KEY');
+$apiKey = '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';
 $sg = new \SendGrid($apiKey);
-//$response = $sg->client->mail()->send()->post($mail);
-try {
-    $response = $sg->client->mail()->send()->post($mail);
-} catch (Exception $e) {
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
-}
+$response = $sg->client->mail()->send()->post($mail);
 echo $response->statusCode();
 echo $response->headers();
 echo $response->body();
 echo "Done"
-
 ?>
