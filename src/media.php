41a9dc36ed09e31b8fbac1688afc8525932f4ef7
Thong Do <thongdo@WBs-MacBook-Pro.local>
2017-04-05 11:52:46 -0700

    new files

--- src/media.php
+++ src/media.php
@@ -11,18 +11,22 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <!-- Optional theme -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat:300,400,400i,600,600i,700,900|Playfair+Display:300,300i,400,400i|Raleway:300,400,400i,700">
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,600,600i|Playfair+Display:300,300i,400,400i|Raleway:300,300i,400,400i,">
 <!--main css-->
 <style type="text/css">
  a {text-decoration: none;}
  *{margin:0 auto;}
 header,footer{background: rgb(9,30,55) center no-repeat fixed;height: 94px;margin: 0 auto;background-size: 100% 100%; background-size: cover;}
 footer{font-family: "Montserrat", sans-serif;color: rgb(255,255,255);font-size: 12px; font-weight: 300; line-height: 1;}
+footer a,a:hover{text-decoration: none; color: #ffffff}
 .container-fluid{margin:0 auto;text-align:center;margin-left: auto;margin-right: auto;padding: 0;}
 .container{width: 960px;margin: 0 auto;padding: 0;text-align:center;margin-left: auto;margin-right: auto;}
 .centered_flex{display: flex;justify-content: center;align-items: center;}
 .centered_nav{display: flex;justify-content: center;align-items: center;}
+.flexcontainer{padding: 0;margin: 0 auto;list-style: none;display: -webkit-box;display: -moz-box;display: -ms-flexbox;display: -webkit-flex;display: flex;-webkit-flex-flow: row nowrap;justify-content: space-around;}
+.flexitem{padding-left: 5px;}
 .nav{font-size: 14px;font-family: "Montserrat", sans-serif;}
+.navbar-default .navbar-nav > li > a {color: #333; padding-left: 32px;}
 nav2 {display: inline-block;padding: 0 15px;position: relative;}
 nav2:hover:before,nav2:hover:after {background: #000;content: "";display: block;height: 1px;position: absolute;top: 50%;width: 12px;}
 nav2:hover:before {right: 90%;}
@@ -34,34 +38,40 @@ toBold{font-weight: 600;}
 .button_wht{background-color: rgb(255, 255, 255);color: rgb(6, 16, 42);}
 .biggerfont{font-family: "Playfair Display",serif;font-size: 38px;}
 .smallerfont{font-size: 20px;}
-.jumbotron p{font-size: 16px;margin: 0 auto;font-weight: 300;font-size: 17px;}
+.jumbotron p{font-size: 16px;margin: 0 auto;font-weight: 300;}
 .jumbotron_wht{background: rgb(255,255,255);}
 .jumbotron_w_bg{background: rgb(248,249,249);background-size: cover;border: 1px solid rgb(235, 235, 235);margin: 0 auto;}
 .movedown2p{margin-top: 2%;}
 .movedown5p{margin-top: 5%;}
 .movedown10p{margin-top: 10%;}
 .lifestyle_word{margin-top: 20%; margin-left: 30%;}
-.ghk_quotes{margin-top: 5%;margin-left: 20%;}
+.ghk_word{text-align: left;}
+.ghk_wording{margin-left: 20px;}
 .textbox{width: 748px;}
-h3{font-family: "Playfair Display",serif;font-size: 38px;line-height: 1;}
-h4{font-size: 20px;line-height: 1;}
+h3{font-family: "Playfair Display",serif;font-size: 38px;line-height: 24px;}
+h4{font-size: 20px;}
+h5{font-size: 13px;font-weight: 300;}
 body,html {box-sizing: border-box;margin:0px;padding:0px;overflow-x: hidden;}
-body{text-align: center;font-family: "Raleway",sans-serif;font-size: 16px; font-weight: 300;}
-@media screen and (min-width: 1199px){.socialicons{margin-left: 54px;}}
+body{text-align: center;font-family: "Raleway",sans-serif;font-size: 16px; font-weight: 300; line-height: 24px;}
+@media screen and (min-width: 1199px){.socialicons{margin-left: 4px;}}
 @media screen and (max-width:1199px){.jumbotron_w_bg .textbox{width: 80%} .ghk_quotes{margin-left: 20%;} .lifestyle_word{margin-left: 20%;} .img_wrapper{margin-left: 5%;}}
-@media screen and (max-width:991px){.ghk_word{margin-top: 20px;} .jumbotron_w_bg .textbox{width: 100%;} .ghk_quotes{margin-left: 0;} .lifestyle_word{margin: 0;} .img_wrapper{margin:0;}}
-@media screen and (max-width:767px){.centered_nav{display: block;} .img_wrapper img{width: 95%;} .img_wrapper{text-align: center;}}
-@media screen and (max-width:610px){.textbox{width: 80%;}}
-@media screen and (max-width:510px){.textbox{width: 85%;}}
-@media screen and (max-width:494px){.textbox{width: 95%;}}
-@media screen and (max-width:469px){img{max-width: 100%; height: auto; width: auto\9;}}
+@media screen and (max-width:991px){.lifestyle_word{margin-left: -10px;} .centered_nav{display: block;} .jumbotron_w_bg .textbox{width: 100%;} .ghk_quotes{margin-left: 0;} .img_wrapper{margin:0;} .navbar-header {float: none;}
+.navbar-left,.navbar-right {float: none !important;} .navbar-toggle {display: block; margin-right: 30px;} .navbar-collapse {border-top: 1px solid transparent;box-shadow: inset 0 1px 0 rgba(255,255,255,0.1);}
+.navbar-fixed-top {top: 0;border-width: 0 0 1px;} .navbar-collapse.collapse {display: none!important;} .navbar-nav {float: none!important;margin-top: 7.5px;} .navbar-nav>li {float:none;}
+.navbar-nav>li>a {padding-top: 10px;padding-bottom: 10px;} .collapse.in{display:block !important;}}
+@media screen and (max-width:767px){div.img_wrapper img{max-width: 100%; height: auto; width: auto\9;} div.img_wrapper {padding-left: 30px; padding-right: 30px;} .lifestyle_word{margin: 0;} .navbar-toggle {margin-right: 40px;}
+.img_wrapper{text-align: center;} .ghk_word{margin-top: 10%; margin-left: 25%; margin-right: 30px;} .textbox{width: 99%;} footer{padding-left: 30px; padding-right: 30px;}}
+@media screen and (max-width:700px){.ghk_word{margin-left: 12.5%;}}
+@media screen and (max-width:640px){.ghk_word{margin-left: 30px;}}
+@media screen and (max-width:469px){div.magazine_wrapper img{width: 95%;} .magazine_wrapper,.lifestyle_word{padding-left: 30px;padding-right: 30px;}}
+@media screen and (max-width:369px){div.lifestyle_word img{width: 90%;}}
 </style>
 </head>
 <body>
   <!--main content-->
   <header class="centered_flex">
     <div class="container-fluid">
-      <img src="../img/citylogo1.png" alt="city logo">
+      <a href="/"><img src="../img/citylogo1.png" alt="citylogo"></a>
     </div>
   </header>
 
@@ -86,6 +96,7 @@ body{text-align: center;font-family: "Raleway",sans-serif;font-size: 16px; font-
         <li><a href="#"><nav2>SKIN</nav2></a></li>
         <li><a href="#"><nav2>BODY</nav2></a></li>
         <li><a href="media.php"><nav2>MEDIA</nav2></a></li>
+        <li><a href="contact.php"><nav2>CONTACT</nav2></a></li>
         <li><a href="#"><nav2>VIEW CART</nav2></a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
@@ -103,13 +114,28 @@ body{text-align: center;font-family: "Raleway",sans-serif;font-size: 16px; font-
   <div class="container">
     <div class="textbox">
       <div class="row">
-        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <div class="col-lg-3 col-md-2 col-sm-2 col-xs-12">
           <img src="../img/winslet.png" alt="winslet screenshot">
         </div>
-        <div class="col-lg-6 col-lg-pull-1 col-md-6 col-md-pull-1 col-sm-12 col-xs-12">
+        <div class="col-lg-8 col-lg-offset-1 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-12">
           <div class="ghk_word">
-            <div><img src="../img/ghk_logo.png" alt="ghk logo"></div>
-            <div class="ghk_quotes"><img src="../img/ghk_quotes.png" alt="quotes"></div>
+            <div><img src="../img/goodhousekeeping.png"></div>
+            <div class="movedown5p"></div>
+            <div class="row">
+              <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"><img src="../img/quotes.png" alt="quotes"></div>
+              <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 ghk_wording"><p>Want lips this lush? Our testers did, so we asked them to try different treatments for a month, in hopes of releasing their inner Angelina Jolie. THE WINNERS: City Lips® clear gloss Lip Plumping Treatment created the fullest smoochers.</p>
+                <h5>- GOOD HOUSEKEEPING MAGAZINE, PAGE 45.</h5>
+              </div>
+            <div class="clearfix"></div>
+            </div>
+            <div class="movedown2p"></div>
+            <div class="row">
+              <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"><img src="../img/quotes.png" alt="quotes"></div>
+              <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 ghk_wording"><p>...The original winner. City Lips &copy; is still champion.</p>
+                <h5>- GOOD HOUSEKEEPING MAGAZINE.</h5>
+              </div>
+            <div class="clearfix"></div>
+          </div>
           </div>
         </div>
       </div>
@@ -121,10 +147,10 @@ body{text-align: center;font-family: "Raleway",sans-serif;font-size: 16px; font-
   <div class="container">
     <div class="textbox">
       <div class="row">
-        <div class="col-lg-6 col-lg-offset-1 col-md-6 col-md-offset-1 col-sm-12">
-          <img src="../img/magfeature.png" alt="magazine features">
+        <div class="col-lg-6 col-lg-offset-1 col-md-6 col-md-offset-1 col-sm-6 col-xs-12">
+          <div class="magazine_wrapper"><img src="../img/magfeature.png" alt="magazine features"></div>
         </div>
-        <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+        <div class="col-lg-5 col-md-5 col-sm-6 col-xs-12">
           <div class="lifestyle_word"><img src="../img/lifestyle_media.png" alt="lifestyle logo"></div>
         </div>
       </div>
@@ -144,10 +170,10 @@ body{text-align: center;font-family: "Raleway",sans-serif;font-size: 16px; font-
  <div class="container">
    <div class="row">
     <div class="col-lg-8 col-md-12 col-sm-12  col-xs-12">
-      <p>&reg; City Cosmetics 2017. City Lips and City Lash are registered trademarks of City Cosmetics, LLC. All rights reserved.</p>
+      <p>&reg; City Beauty 2017. City Lips and City Lash are registered trademarks of City Beauty, LLC. All rights reserved.</p>
     </div>
     <div class="col-lg-4 col-md-12 ol-sm-12 col-xs-12">
-      <p>PRIVACY POLICY &nbsp|&nbsp TERMS &amp; CONDITIONS &nbsp|&nbsp SITEMAP</p>
+      <p><a href="privacy.php">PRIVACY POLICY</a> &nbsp|&nbsp <a href="terms.php">TERMS &amp; CONDITIONS</a></p>
     </div>
   </div>
   <div class="row">
