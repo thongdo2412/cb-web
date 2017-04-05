bf3e556897937c87649b87f0192c2c1d682f5fb8
Thong Do <thongdo@WBs-MacBook-Pro.local>
2017-04-04 18:42:21 -0700

    new files

--- src/index.php
+++ src/index.php
@@ -42,7 +42,7 @@ toBold{font-weight: 600;}
 .nav{font-size: 14px;font-family: "Montserrat", sans-serif;}
 .jumbotron p{font-size: 16px;margin: 0 auto;font-weight: 300; line-height: 24px;}
 .jumbotron_wht{background: rgb(255,255,255);}
-.jumbotron_w_bg{height: 528px;background-image: url("../img/gift_bg.png");border: 1px solid rgb(235, 235, 235);background-repeat:no-repeat;background-size:100% cover;background-position:center;}
+.jumbotron_w_bg{height: 528px;background-image: url("../img/gift_bg.png");border: 1px solid rgb(235, 235, 235);background-size:100% cover;background-position:center;}
 .b3textbox{width:382px;background-color:rgb(255,255,255); text-align:center;}
 .movedown16{margin-top: 16px;}
 .movedown20{margin-top: 20px;}
@@ -53,9 +53,13 @@ toBold{font-weight: 600;}
 .b2margin{margin-left: 17%;}
 .word_wrapper{text-align: left; margin-top: 22%;}
 .b2word_wrapper{text-align: left; margin-top: 28%;}
+.bfaft_wrapper{margin-top: 10%;}
 .updateform{text-align: left;}
+.form-control{border-radius: 0; width: 190px;height: 30px;}
 .cream_wrapper{margin-top: 10%;}
 .productwrapper{}
+.carousel-control.left,.carousel-control.right{background-image: none;}
+.arrowWrapper{ position: relative;top: 50%;transform: translateY(-50%);}
 div.productwrapper .button_wht{display: none;}
 div.productwrapper:hover .button_wht {display: flex;}
 .button_wht:hover {background-color: rgb(6, 16, 42);color: rgb(255, 255, 255);}
@@ -71,12 +75,19 @@ body{text-align: center;font-family: "Montserrat",sans-serif;font-size: 16px; fo
 /*handle responsive for different screens*/
 @media screen and (min-width: 1199px){.socialicons{margin-left: 74px;}}
 @media screen and (max-width:1199px) {.b2margin{margin-left: 7%;} .socialicons{margin:0;} .cream_wrapper{margin-top: 12%;margin-left: 4%;}}
-@media screen and (max-width:991px) {.cream_wrapper{margin-left: 0;} .centered_nav{display: block;float: right;} .b2margin{margin: 0 auto;} .navbar-header {float: none;}
+@media screen and (max-width:991px) {.cream_wrapper{margin-left: 0;margin-top: 16%;} .centered_nav{display: block;} .b2margin{margin: 0 auto;} .navbar-header {float: none;}
 .navbar-left,.navbar-right {float: none !important;} .navbar-toggle {display: block;} .navbar-collapse {border-top: 1px solid transparent;box-shadow: inset 0 1px 0 rgba(255,255,255,0.1);}
 .navbar-fixed-top {top: 0;border-width: 0 0 1px;} .navbar-collapse.collapse {display: none!important;} .navbar-nav {float: none!important;margin-top: 7.5px;} .navbar-nav>li {float:none;}
 .navbar-nav>li>a {padding-top: 10px;padding-bottom: 10px;} .collapse.in{display:block !important;}}
-@media screen and (max-width:767px){.word_wrapper,.b2word_wrapper{margin-top: 0;margin-left: 25%; margin-bottom: 8%;} p.emailinput input{margin-left: 65px;}}
-@media screen and (max-width:479px){.word_wrapper,.b2word_wrapper{margin-left: 12.5%; margin-bottom: 5%;}}
+@media screen and (max-width:767px){.navbar-toggle{margin-right: 45px;} .word_wrapper{margin-top: 16%;margin-left: 25%;} .b2word_wrapper{margin-top: 0;margin-left: 25%;}
+.bfaft_wrapper{margin-top: 0;} .b2word_wrapper{margin-top: 25%;margin-left: 30%;} .b3textbox{margin-left: 15px;margin-right: 15px;} .form-control{width: 100%;height: 100%;} .updateform{margin-left: 30px; margin-right: 30px;}
+div.form-group input{margin: 0;} .submitbtn{float: right;} footer .container{margin-left: 30px;margin-right: 30px;}}
+@media screen and (max-width:606px){.word_wrapper{margin-top: 16%;}}
+@media screen and (max-width:572px){.word_wrapper{margin-left: 14%;} .b2word_wrapper{margin-left: 15%;} .bfaft_wrapper{margin-left: -30px;}}
+@media screen and (max-width:542px){.undereye{margin-right: 40px;} .bfaft_wrapper>img{width: 220%;}}
+@media screen and (max-width:450px){.word_wrapper{margin-left: 30px;} .b2word_wrapper{margin-left: 15px;margin-top: 12%;} .undereye{margin-right: 50px;} .bfaft_wrapper{margin-left: -20px;} .bfaft_wrapper>img{width: 200%;}}
+@media screen and (max-width:414px){.undereye{margin-right: 45px;}}
+@media screen and (max-width:399px){.hidden-xxs{display: none} .col-xxs-12{width: 100%;}.word_wrapper,.b2word_wrapper{margin-top: 0;} }
 @media screen and (min-width:330px) and (max-width:628px){.lip_wrapper{margin-right: 12%;}}
 </style>
 </head>
@@ -120,7 +131,7 @@ body{text-align: center;font-family: "Montserrat",sans-serif;font-size: 16px; fo
  <div class="jumbotron jumbotron_wht">
   <div class="container">
     <div class="row">
-      <div class="col-lg-4 col-lg-offset-2 col-md-5 col-md-offset-1 col-sm-6 col-sm-offset-0">
+      <div class="col-lg-5 col-lg-offset-2 col-md-5 col-md-offset-1 col-sm-6 col-sm-offset-0 col-xs-8 col-xs-offset-0 col-xxs-12">
         <div class="word_wrapper">
           <h3>New!</h3>
           <p></p>
@@ -130,16 +141,17 @@ body{text-align: center;font-family: "Montserrat",sans-serif;font-size: 16px; fo
           <p></p>
           <div class="movedown20"></div>
           <h4>WITH UNDER EYE RECOVERY</h4>
+          <p></p>
           <h4>BY CITY BEAUTY</h4>
           <div class="movedown20"></div>
         <div class="button button_blue pull-left"><a href="undereye.php">DISCOVER</a></div>
         <div class="clearfix"></div>
       </div>
     </div>
-      <div class="col-lg-2 col-lg-offset-1 col-md-2 col-sm-3">
-        <img src="../img/undereye.png" alt="undereye cream">
+      <div class="col-lg-2 col-md-2 col-sm-3 col-xs-1 hidden-xxs">
+        <div class="undereye"><img src="../img/undereye.png" alt="undereye cream"></div>
     </div>
-      <div class="col-lg-2 col-md-2 col-sm-3 cream_wrapper">
+      <div class="col-lg-2 col-md-2 col-sm-2 hidden-xs cream_wrapper">
         <img src="../img/cream.png" alt="cream">
     </div>
     </div>
@@ -151,7 +163,7 @@ body{text-align: center;font-family: "Montserrat",sans-serif;font-size: 16px; fo
 <div class="jumbotron jumbotron_wht">
  <div class="container">
    <div class="row b2margin">
-     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 col-xxs-12">
        <div class="b2word_wrapper">
          <h3>City Lips</h3>
          <p></p>
@@ -160,11 +172,11 @@ body{text-align: center;font-family: "Montserrat",sans-serif;font-size: 16px; fo
          <div class="button button_blue pull-left">SHOP NOW</div>
        </div>
      </div>
-     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+     <div class="col-lg-2 col-md-2 col-sm-2 col-sm-pull-1 hidden-xs">
        <div class="lip_wrapper"><img src="../img/citylips.png" alt="citylips"></div>
      </div>
-     <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 movedown2p">
-       <img src="../img/bf_aft.png" alt="before n after lips">
+     <div class="col-lg-3 col-md-3 col-sm-4 col-sm-pull-1 col-xs-3 col-xs-pull-0 hidden-xxs">
+       <div class="bfaft_wrapper"><img src="../img/bf_aft.png" alt="before n after lips"></div>
      </div>
    </div>
  </div>
@@ -186,25 +198,64 @@ body{text-align: center;font-family: "Montserrat",sans-serif;font-size: 16px; fo
  <div class="container">
    <h3>Top Sellers</h3>
    <div class="row movedown5p">
-     <div class="col-xs-6 col-sm-3 col-md-3 productwrapper">
+     <div class="hidden-xs col-sm-3 col-md-3 productwrapper">
        <img src="../img/rated_citylip.png" alt="rated_lips">
        <div class="movedown5p"></div>
        <div class="button button_wht">SHOP NOW</div>
      </div>
-     <div class="col-xs-6 col-sm-3 col-md-3 productwrapper">
+     <div class="hidden-xs col-sm-3 col-md-3 productwrapper">
        <img src="../img/rated_dkspot.png" alt="rated dark spot">
        <div class="movedown5p"></div>
        <div class="button button_wht">SHOP NOW</div>
      </div>
-     <div class="col-xs-6 col-sm-3 col-md-3 productwrapper">
+     <div class="hidden-xs col-sm-3 col-md-3 productwrapper">
        <img src="../img/rated_citylash.png" alt="rated city lash">
        <div class="movedown5p"></div>
        <div class="button button_wht">SHOP NOW</div>
      </div>
-     <div class="col-xs-6 col-sm-3 col-md-3 productwrapper">
+     <div class="hidden-xs col-sm-3 col-md-3 productwrapper">
        <img src="../img/rated_undereye.png" alt="rated under eye">
        <div class="movedown5p"></div>
        <div class="button button_wht"><a href="undereye.php">SHOP NOW</a></div>
+     </div>
+     <div class="visible-xs productwrapper">
+       <div id="myCarousel" class="carousel slide" data-ride="carousel">
+         <!-- Wrapper for slides -->
+         <div class="carousel-inner" role="listbox">
+           <div class="item active">
+             <img src="../img/rated_citylip.png" alt="rated_lips">
+             <div class="movedown5p"></div>
+             <div class="button button_wht">SHOP NOW</div>
+           </div>
+
+           <div class="item">
+             <img src="../img/rated_dkspot.png" alt="rated dark spot">
+             <div class="movedown5p"></div>
+             <div class="button button_wht">SHOP NOW</div>
+           </div>
+
+           <div class="item">
+             <img src="../img/rated_citylash.png" alt="rated city lash">
+             <div class="movedown5p"></div>
+             <div class="button button_wht">SHOP NOW</div>
+           </div>
+
+           <div class="item">
+             <img src="../img/rated_undereye.png" alt="rated under eye">
+             <div class="movedown5p"></div>
+             <div class="button button_wht"><a href="undereye.php">SHOP NOW</a></div>
+           </div>
+         </div>
+
+         <!-- Left and right controls -->
+         <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+           <div class="arrowWrapper"><img src="../img/prevArr.png"></div>
+         </a>
+         <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+           <div class="arrowWrapper"><img src="../img/nextArr.png"></div>
+         </a>
+         <div class="clearfix"></div>
+       </div>
      </div>
    </div>
  </div>
@@ -215,7 +266,7 @@ body{text-align: center;font-family: "Montserrat",sans-serif;font-size: 16px; fo
  <div class="container">
    <div class="updateform">
    <div class="row">
-     <div class="col-xs-10 col-sm-12 col-md-8 col-lg-7 col-lg-offset-2 col-md-offset-2 col-sm-offset-1 col-xs-offset-1">
+     <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-lg-offset-2 col-md-offset-1 col-sm-offset-0">
        <h3>Stay Up to Date</h3>
        <div class="movedown16"></div>
        <h4>WE HAVE SOME EXCITING THINGS PLANNED FOR 2017 - </h4>
@@ -224,22 +275,20 @@ body{text-align: center;font-family: "Montserrat",sans-serif;font-size: 16px; fo
          <p>It's starting this Spring, so don't miss out.</p>
         <div class="movedown20"></div>
        <p><b>Sign up below for instant notifications and get a sneak peak at what's ahead.</b></p>
+       <div class="movedown20"></div>
+       <form class="form-inline">
+         <div class="form-group">
+           <p>FIRST NAME*<input type="text" class="form-control" id="name"></p>
+         </div>
+         <div class="form-group">
+           <p class="emailinput">EMAIL*<input type="email" class="form-control" id="email"></p>
+         </div>
+         <button type="submit" class="submitbtn button_blue">SUBMIT</button>
+       </form>
+       <div class="clearfix"></div>
      </div>
    </div>
-     <div class="row">
-       <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-lg-offset-2 col-md-offset-2 col-sm-offset-1 col-xs-offset-1">
-         <form class="form-inline movedown20">
-           <div class="form-group">
-             <p>FIRST NAME*<input type="text" class="form-control-static" id="name"></p>
-           </div>
-           <div class="form-group">
-             <p class="emailinput">EMAIL*<input type="email" class="form-control-static" id="email"></p>
-           </div>
-           <button type="submit" class="submitbtn button_blue">SUBMIT</button>
-         </form>
-       </div>
-     </div>
-   </div>
+  </div>
   </div>
 </div>
 <!-- footer -->
