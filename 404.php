dfc57bf1e0333c7602f007f962c6aa1263e57455
Thong Do <dhthong@gmail.com>
2017-06-20 20:40:48 -0700

    new files

---
+++ 404.php
@@ -0,0 +1,25 @@
+<!-- include PHP template header file-->
+<?php include("head.php"); ?>
+<title>404 - Page Not Found</title>
+
+<style type="text/css">
+  .maincontent{text-align: left;}
+  body{font-family: "Raleway",sans-serif;}
+  .cruel-free{margin-left: -10px;}
+  /*Responsive for different screen*/
+  @media screen and (min-width: 1199px) {.social-wrap{margin-left: 62px;}}
+</style>
+</head>
+<body>
+  <!--head and navigation content-->
+<?php include ("header.php"); ?>
+<div class="jumbotron jumbotron_wht">
+  <div class="container">
+    <img src="../img/error-404.png" alt="error">
+  </div>
+</div>
+
+  <!--include PHP footer template -->
+  <?php include ("footer.php"); ?>
+  </body>
+  </html>
