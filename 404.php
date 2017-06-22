089a50e7ff708ee71db560429b6a4776f8fdeb11
Thong Do <dhthong@gmail.com>
2017-06-21 19:08:16 -0700

    new files

--- 404.php
+++ 404.php
@@ -5,31 +5,34 @@
 <style type="text/css">
   body{font-family: "Montserrat", sans-serif;}
   .cruel-free{margin-left: -4px;}
-  .text{font-family: 'Poiret One', cursive;font-size: 20px; position: absolute; top: 130px;left:100px; width: 600px;}
-  .image {position: relative; width: 100%; /* for IE 6 */}
-  .image img{height: 500px;position: relative;left: 120px;}
-  .linebreak5{margin-top: 40px;}
+  .text{font-family: 'Poiret One', cursive;font-size: 20px;margin-top: 40px;}
+  .linebreak5{margin-top: 30px;}
+  .cream-swatch img{width: 300px;}
   a{color: #e1c37b; }
   a:hover {color: #e1c37b;}
-  h1{font-size: 40px;}
+  h2{font-size: 40px;}
   /*Responsive for different screen*/
   @media screen and (min-width: 1199px) {.social-wrap{margin-left: 42px;}}
-  @media screen and (max-width: 1199px){.image img{max-width: 100%; height: auto; width: auto\9;}}
+  @media screen and (max-width: 767px){.text{margin-top: 0; margin-left: 20px;margin-right: 20px;}}
 </style>
 </head>
 <body>
   <!--head and navigation content-->
 <?php include ("header.php"); ?>
+<div class="jumbotron jumbotron_wht">
   <div class="container">
-    <div class="image">
-      <img src="../img/cream.jpg" alt="cream" />
-      <div class="text">
-        <h1>We're sorry that you landed on this page due to a broken link.</h1>
+    <div class="row">
+      <div class="col-lg-6 col-lg-offset-2 col-md-7 col-md-offset-1 col-sm-11 col-sm-offset-0 text">
+        <h2>We're sorry that you landed on this page due to a broken link.</h2>
         <div class="linebreak5"></div>
-        Please go back to <a href="/">City Beauty</a> home page or use the navigation bar above to go to product pages.
+        You can either go back to <a href="/">City Beauty</a> homepage or use the navigation bar above to go to the product pages.
+      </div>
+      <div class="col-lg-2 cream-swatch">
+        <img src="../img/cream1.jpg" alt="cream" />
       </div>
     </div>
   </div>
+</div>
 
   <!--include PHP footer template -->
   <?php include ("footer.php"); ?>
